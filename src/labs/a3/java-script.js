ae40b66f95e3257787ac2408d4f8ad0526dedeac
Casca Kwok <casca.kwok@gmail.com>
2023-07-20 19:32:16 -0700

    JavaScript and React.js Assignment 3

---
+++ src/labs/a3/java-script.js
@@ -0,0 +1,21 @@
+import VariablesAndConstants from "./variables-and-constants";
+import VariableTypes from "./variable-types.js";
+import BooleanVariables from "./boolean-variables.js";
+import IfElse from "./if-else.js";
+import TernaryOperator from "./ternary-operator.js"
+
+function JavaScript() {
+    console.log('Hello World!');
+    return(
+       <div>
+          <h1>JavaScript</h1>
+          <VariablesAndConstants/>
+          <VariableTypes/>
+          <BooleanVariables/>
+          <IfElse/>
+          <TernaryOperator/>
+       </div>
+    );
+ }
+ export default JavaScript
+ 
