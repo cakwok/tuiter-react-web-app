2acb3f51f20750eed7a8f7abc3ccdf3dc7bb3fdf
Casca Kwok <casca.kwok@gmail.com>
2023-07-23 13:13:31 -0700

    JavaScript and React.js Assignment 3

---
+++ src/tuiter/explore-screen.js
@@ -0,0 +1,12 @@
+import TuitSummaryItem from "./tuit-summary-list/tuit-summary-item"
+import TuitSummaryList from "./tuit-summary-list";
+
+function ExploreScreen() {
+    return (
+        <div>
+            <TuitSummaryList/>
+        </div>
+    );
+   }
+export default ExploreScreen;
+   
