3b8a8082ec4ab73bf3a2623f55f91ce3f665924c
Casca Kwok <casca.kwok@gmail.com>
2023-08-01 07:48:33 -0700

    a4 State and Redux su23

--- src/tuiter/tuits/tuit-item.js
+++ src/tuiter/tuits/tuit-item.js
@@ -3,21 +3,9 @@ import { FaCheckCircle, FaComment, FaRetweet, FaRegHeart, FaHeart, FaShare } fro
 import {useDispatch} from "react-redux";
 import { deleteTuit } from "./tuits-reducer"; 
 import { FaTimes } from 'react-icons/fa';
+import TuitStats from "./tuit-stats"
 
 const TuitItem = ({ tuit }) => {
-  const [liked, setLiked] = useState(tuit.liked);
-  const [likes, setLikes] = useState(tuit.likes);
-
-  const handleLikeClick = () => {
-    if (liked) {
-      setLiked(false);
-      setLikes(likes - 1);
-    } else {
-      setLiked(true);
-      setLikes(likes + 1);
-    }
-  };
-
   const dispatch = useDispatch();
   const deleteTuitHandler = (id) => {
     dispatch(deleteTuit(id));
@@ -42,26 +30,9 @@ const TuitItem = ({ tuit }) => {
        <div>{tuit.tuit}</div>
      </div>
    </div>
-   
-   <div className="row" style={{ marginTop: '20px' }}>
-     <div className="col-2"></div>
-     <div className="col-2">
-        <span><FaComment />  </span>{tuit.replies} 
-     </div>
-     <div className="col-2">
-        <span><FaRetweet />  </span>{tuit.retuits} 
-     </div>
-     <div className="col-2">
-        <span>
-          {liked ? (<FaHeart style={{ color: 'red', cursor: 'pointer'}} 
-                              onClick={handleLikeClick} />) : (
-                    <FaRegHeart style={{ color: 'red', cursor: 'pointer'}} 
-                                onClick={handleLikeClick} />)}  </span>{likes} 
-     </div>
-     <div className="col-2">
-        <span><FaShare />  </span> 
-     </div>
-   </div>
+  
+    <TuitStats key={tuit._id}
+        tuit={tuit}/>
   </li>
  );
 };
