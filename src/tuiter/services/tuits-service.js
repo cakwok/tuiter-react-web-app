0a16f666ea78d79915677eb3c9b889985b2602fb
Casca Kwok <casca.kwok@gmail.com>
2023-08-08 11:44:03 -0700

    a5 REST su23

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
 /*const TUITS_API = 'http://localhost:4000/api/tuits';*/
-const TUITS_API = 'https://tuiter-node-server-app-1vve.onrender.com/api/tuits';
-/*const API_BASE = process.env.REACT_APP_API_BASE;
+/*const TUITS_API = 'https://tuiter-node-server-app-1vve.onrender.com/api/tuits';*/
+const API_BASE = process.env.REACT_APP_API_BASE;
 console.log("API_BASE", API_BASE);
-console.log("process.env", process.env);*/
-/*const TUITS_API = `${API_BASE}/tuits`;*/
+const TUITS_API = `${API_BASE}/tuits`;
 
 
 export const createTuit = async (tuit) => {
@@ -20,6 +19,7 @@ export const findTuits = async () => {
    }
    
 export const deleteTuit = async (tid) => {
+  console.log(TUITS_API);
     const response = await axios.delete(`${TUITS_API}/${tid}`)
     return response.data
   }
