29db055eddd736fbaadcc491a8b647a64ac5c4a8
Casca Kwok <casca.kwok@gmail.com>
2023-08-08 09:21:40 -0700

    a5 REST su23

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 /*const TUITS_API = 'http://localhost:4000/api/tuits';*/
-/*const TUITS_API = 'https://tuiter-node-server-app-1vve.onrender.com/api/tuits';*/
+const TUITS_API = 'https://tuiter-node-server-app-1vve.onrender.com/api/tuits';
 const API_BASE = process.env.REACT_APP_API_BASE;
-const TUITS_API = `${API_BASE}/tuits`;
+console.log(API_BASE);
+console.log(process.env);
+/*const TUITS_API = `${API_BASE}/tuits`;*/
 
 
 export const createTuit = async (tuit) => {
