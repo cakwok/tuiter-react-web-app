5986bc362e3e9376c0380e486baca052e27a886c
Casca Kwok <casca.kwok@gmail.com>
2023-08-18 14:18:08 -0700

    project

--- src/project/search.js
+++ src/project/search.js
@@ -3,51 +3,32 @@ import { useEffect, useState } from "react";
 
 function Search() {
 
-    console.log("process.env.REACT_APP_YELP_API_KEY", process.env.REACT_APP_YELP_API_KEY);
-    
     const [results, setResults] = useState({});
-    const url = `https://api.yelp.com/v3/businesses/search?location=milpitas&sort_by=best_match&limit=20`;
-    const hkurl = 'https://geodata.gov.hk/gs/api/v1.0.0/locationSearch?q=cultural%20centre';
-    const webex_url = 'https://webexapis.com/v1/rooms';
+    const [restaurants, setRestaurants] = useState({});
+
     const apiUrl = 'http://localhost:4000/proxy/yelp';
 
-
     const search = async () => {
-        
-        /*const response = await axios.get(url, {
-            headers: {
-                Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
-                accept: 'application/json',
-                'Access-Control-Allow-Origin': '*',
-              },
-            params: {
-            location: 'milpitas',
-            sort_by: 'best_match',
-            limit: 20,
-            },
-        });
-        */
 
         const params = {
-            /*path: '/search', // The Yelp API endpoint*/
-            location: 'milpitas',
+            location: 'bay area',
             sort_by: 'best_match',
-            limit: 20,
           };
-          
 
-        console.log("apiUrl", apiUrl);
-        console.log("params", params);
+       const response = await axios.get(apiUrl, { params });
+       
+       const results  = response.data;
+        
+       const modifiedResults = {
+            ...response.data,
+            businesses: results.businesses.map((business) => ({
+                ...business,
+                image_url: business.image_url.replace(/o\.jpg$/, "348s.jpg")
+            }))
+        };
 
-        axios.get(apiUrl, { params })
-        .then(response => {
-            console.log("response.data", response.data);
-            const results = response.data;
-            setResults(results);
-        })
-        .catch(error => {
-            console.error(error);
-        });
+        setResults(modifiedResults);
+
     };
 
     useEffect(() => {
@@ -58,7 +39,27 @@ function Search() {
         <div>
           <h2>Search</h2>
           <pre>{JSON.stringify(results, null, 2)}</pre>
+
+          <h2>Restaurants</h2>
+          <div className="table-responsive">
+            <table className="table">
+                <tbody>
+                    <tr>
+                    {results.businesses.map((business) => (
+                            
+                            <td>
+                                <div key={business.id}>
+                                    {business.name}
+                                    <img src={business.image_url} width="300" /> 
+                                    
+                                </div>
+                            </td>
+                    ))}
+                    </tr>
+                </tbody>
+            </table>
+          </div>
         </div>
     );
-    }
+}
 export default Search;
--- src/project/service.js
+++ src/project/service.js
@@ -1,3 +1,8 @@
 
+const apiUrl = 'http://localhost:4000/proxy/yelp';
 
+export const fullSearch = async (query) => {
+    const response = await axios.get(apiUrl, { params });
+    return response.data;
+}
 
